bbe2a49ee0856df17751624a95b0dc3486a5f931
应承峻 <1052257712@qq.com>
2019-05-18 15:04:05 +0800

    new project

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,7 +1,31 @@
 package code;
 
 public class Main {
+
+    /*
+    * [代码风格约定]
+    * 函数命名采用下划线法，示例：void build_red_black_tree();
+    * 变量命名采用小驼峰，示例：indexValue
+    * 每个类需要提供相应的接口，并对接口进行注释
+    * 每个类中的方法需要进行注释
+    * 单行注释统一采用//
+    * 函数类定义的左大括号不换行，示例： void build_red_black_tree() {
+    * 变量赋值、运算符预留空格，示例：int a = b + 5;
+    * */
+
+    /*
+    * [示例]
+    * function: 主方法
+    * param[in]: 命令行参数数组args(如果是void则省略该行)
+    * param[out]: (如果是void则省略该行)
+    * author: ycj (采用个人姓名缩写)
+    * note: 这是一个示例(如果没有特殊说明则省略该行)
+    * */
+
     public static void main(String[] args) {
         System.out.println("Minisql!");
     }
+
+
+
 }
