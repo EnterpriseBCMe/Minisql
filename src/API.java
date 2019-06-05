b671b50b0674fe6d126ca2620fdeafac2721dfa7
应承峻 <1052257712@qq.com>
2019-06-05 20:32:26 +0800

    整理现有成果

---
+++ src/API.java
@@ -0,0 +1,43 @@
+import CATALOGMANAGER.Table;
+import INDEXMANAGER.Index;
+import RECORDMANAGER.TableRow;
+
+public class API {
+
+    public static boolean createTable(String tabName, Table tab) {
+
+        return true;
+    }
+
+    public static boolean dropTable(String tableName) {
+
+        return true;
+    }
+
+    public static boolean createIndex(Index index) {
+
+        return true;
+    }
+
+    public static boolean dropIndex(Index index) {
+
+        return true;
+    }
+
+    public static boolean insertRow(String tabName, TableRow row) {
+
+        return true;
+    }
+
+    public static int deleteRow(TableRow row) {
+
+        return 0;
+    }
+
+    //public static Vector<TableRow> select(String tabName,Vector<String> attrName, )
+
+
+
+
+
+}
